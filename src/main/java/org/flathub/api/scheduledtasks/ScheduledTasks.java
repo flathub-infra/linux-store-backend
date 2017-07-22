9bcd2fa48fd7e71eeb01ac5304803f029879a377
Jorge García <jgarciao@gmail.com>
2017-07-22 07:38:35 +0200

    Execute ScheduledTask only if SCHED profile is active

--- src/main/java/org/flathub/api/scheduledtasks/ScheduledTasks.java
+++ src/main/java/org/flathub/api/scheduledtasks/ScheduledTasks.java
@@ -4,12 +4,14 @@ import org.flathub.api.service.UpdateService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @Transactional
+@Profile("SCHED")
 public class ScheduledTasks {
 
   private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
