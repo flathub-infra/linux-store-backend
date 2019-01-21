14ad1e4037b33da1b180563eae4ad3c0b2188cde
Alexander Larsson <alexl@redhat.com>
2019-01-21 12:59:18 +0100

    initial migration: Drop options not in psql 9.2

--- src/main/resources/db/migration/V1/V1.0__Initial_version.sql
+++ src/main/resources/db/migration/V1/V1.0__Initial_version.sql
@@ -8,13 +8,13 @@
 -- Started on 2018-01-17 07:25:58 CET
 
 SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
+-- SET lock_timeout = 0;
+-- SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SET check_function_bodies = false;
 SET client_min_messages = warning;
-SET row_security = off;
+-- SET row_security = off;
 
 --
 -- TOC entry 1 (class 3079 OID 12390)
